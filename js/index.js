3c6d2b138a14f5e6c2cee9675a700bceec9b2b06
Josivan Whesley <jowhesley03@gmail.com>
2022-09-12 22:33:55 -0300

    inserir o keypress e o keyfocus e a funcao que recebe apenas numeros no campo valor

--- js/index.js
+++ js/index.js
@@ -1,20 +1,29 @@
+var nomeProduto = document.getElementById('produto');
+var preco = document.getElementById('valor');
+var tipoTransacao = document.getElementById('opcao');
+var padraoNumero = /[^0-9(),.]+/g;
+
+
 function validarForm(e){
     e.preventDefault();
-    var nomeProduto = document.getElementById('produto');
-    var preco = document.getElementById('valor');
-    var tipoTransacao = document.getElementById('opcao');
-    var padraoNumero = /[^0-9(),.]+/g;
 
     if(nomeProduto.value == ""){
         alert('Digite o nome do Produto!');
         nomeProduto.focus();
         return
     }
-
     if(padraoNumero.test(preco.value)){
         alert('Digite o valor com números!')
         preco.focus();
-        
         return
     }
 }
+
+function testarCampoValor(e){
+    e.preventDefault()
+    console.log(e)
+
+    if((/[0-9]/g).test(e.key)){
+        e.target.value += e.key
+    }
+    }
