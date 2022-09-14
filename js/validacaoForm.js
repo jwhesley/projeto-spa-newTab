2b30ba889ee1bb8fed52f646b0782cacdf82e3a7
Josivan Whesley <jowhesley03@gmail.com>
2022-09-13 21:54:00 -0300

    fiz a alteracao do nome do arquivo JS e a identacao do HTML, CSS E JS

--- js/validacaoForm.js
+++ js/validacaoForm.js
@@ -3,45 +3,37 @@ var preco = document.getElementById('valor');
 var tipoTransacao = document.getElementById('opcao');
 var padraoNumero = /[^0-9(),.]+/g;
 
-
-
-function validarForm(e){
+function validarForm(e) {
     e.preventDefault();
-
-    if(nomeProduto.value == ""){
+    if (nomeProduto.value == "") {
         alert('Digite o nome do Produto!');
         nomeProduto.focus();
         return
     }
-    if(padraoNumero.test(preco.value)){
+    if (padraoNumero.test(preco.value)) {
         alert('Digite o valor com números!')
         preco.focus();
         return
     }
 }
 
-
-
-function testarCampoValor(e){
-    
-    if(padraoNumero.test(e.key)){
+function testarCampoValor(e) {
+    if (padraoNumero.test(e.key)) {
         e.preventDefault()
         return
     }
-    if(!e.target.value) return;
+    if (!e.target.value) return;
 
     valor = e.target.value.toString();
     valor = valor.replace(/[\D]+/g, '');
     valor = valor.replace(/([0-9]{1})$/g, ",$1")
 
-    if(valor.length >= 6){
-        while(/([0-9]{4})[,|\.]/g.test(valor)){
+    if (valor.length >= 6) {
+        while (/([0-9]{4})[,|\.]/g.test(valor)) {
             valor = valor.replace(/([0-9]{1})$/g, ",$1");
-			valor = valor.replace(/([0-9]{3})[,|\.]/g, ".$1");
+            valor = valor.replace(/([0-9]{3})[,|\.]/g, ".$1");
         }
 
     }
     e.target.value = valor
 }
-
-
