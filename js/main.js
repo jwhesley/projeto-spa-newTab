d501c4673ee2cda25c8f620b788bd135001d6a84
Josivan Whesley <jowhesley03@gmail.com>
2022-09-22 07:51:22 -0300

    ajustando o css com Javascript

--- js/main.js
+++ js/main.js
@@ -2,27 +2,31 @@ var mercadoria = [
     {
         produto: 'Melão',
         valor: 'R$ 9,99',
-        transacao: true
+        transacao: 'compra'
     },
     {
         produto: 'Mamão',
         valor: 'R$ 5,99',
-        transacao: false
+        transacao: 'venda'
     },
     {
         produto: 'Goiaba',
         valor: 'R$ 1,99',
-        transacao: true
+        transacao: 'compra'
     },
 ]
 for (mercado in mercadoria){
+
+    
     document.querySelector('table.lista tbody').innerHTML += (
     `<tr>
-    
         <td>
+        ${(mercadoria[mercado].transacao == "compra" ? "-" : "+") }
+        </td>
+        <td class="tdDinamico1">
             ${mercadoria[mercado].produto}
         </td>
-        <td>
+        <td class="tdDinamico2">
             ${mercadoria[mercado].valor}
         </td>   
     
