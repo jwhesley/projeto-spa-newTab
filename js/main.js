af3e6674e15728ec827b504369c1c10a94f64008
Josivan Whesley <jowhesley03@gmail.com>
2022-09-22 21:58:45 -0300

    continuando quebrando a cabeça no js

--- js/main.js
+++ js/main.js
@@ -19,23 +19,30 @@ var mercadoria = [
         valor: 'R$ 2,79',
         transacao: 'Venda'
     },
-]
-for (mercado in mercadoria){
+];
 
-    
-    document.querySelector('table.lista tbody').innerHTML += (
-    `<tr>
-        <td>
-        ${(mercadoria[mercado].transacao == "compra" ? "-" : "+") }
-        </td>
-        <td class="tdDinamico1">
-            ${mercadoria[mercado].produto}
-        </td>
-        <td class="tdDinamico2">
-            ${mercadoria[mercado].valor}
-        </td>   
-    
-    </tr>
-    `)
+function desenhaTabela(){
+    let total = 0;
+
+    linhasExistentes = [...document.querySelectorAll('table.lista tbody .dinamico')];
+    linhasExistentes.forEach((element) => {
+        element.remove()
+    });
+    for (mercado in mercadoria){
+        document.querySelector('table.lista tbody').innerHTML += (
+        `<tr class="dinamico">
+            <td>
+            ${(mercadoria[mercado].transacao == "compra" ? "-" : "+") }
+            </td>
+            <td class="tdDinamico1">
+                ${mercadoria[mercado].produto}
+            </td>
+            <td class="tdDinamico2">
+                ${mercadoria[mercado].valor}
+            </td>   
+        
+        </tr>
+        `)
+    }
 }
-
+desenhaTabela()
